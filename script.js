9dc902053c6ed760f56ae925c95ad6a42cf8d195
Boi The <boithe@Bois-MacBook-Pro.local>
2024-11-07 22:25:49 -0600

    more fuctions

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ document.getElementById("tellFortuneButton").addEventListener("click", tellFortu
 
 function tellFortune() {
     const nameText = document.getElementById('nameText').value;
-
     const places = "Aika, Lotaw, Lovaw, Ccangho, Pangva, Ramcci, Sihanthung, Zawngnak, Angraw, Polei, Vuakkhipaw, Lavoikum, Darcung, Khawboi, Setlai, Lungkhin, Leipi, Calthawng, Langly, Sensi, Khawtua, Tuinia, Rovaw, Rezua, Ccawtui, Ransae, Etang, Thandya, Tuibyn, Hrinthang, Siangaw, Lungthlialia, Thawlang, Hunglei, Raso, Tuilaw, Tingsi, Zesaw, Thesi, Lungring, Sungpi, Votui, Kailung, Belae, Lungngo, Sempi, Tuphae, Lungdua, Suiton, Daidin, Din, Voiru, Narbung";
+
+    const alphabetArray = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
+    const randomLetter = alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
 
     // Convert the string into an array and select a random place
     const placesArray = places.split(', ').map(place => place.trim());
@@ -15,10 +17,11 @@ function tellFortune() {
             "Look for love in " + randomPlace + ", Zotung",
             "Your true love hails from " + randomPlace + ", Zotung",
             "Find your soulmate in " + randomPlace + ", Zotung",
-            "Your partner is waiting for you in " + randomPlace + ", Zotung"
+            "Your partner is waiting for you in " + randomPlace + ", Zotung",
+            "Sorry ;(, seems like you will never find one."
         ];
         let randomGenMgs = messages[Math.floor(Math.random() * messages.length)];
-        const randomMessage = nameText + ", " + randomGenMgs + ", Chin.";
+        const randomMessage = nameText + ", " + randomGenMgs + ", Chin. and starts with '" + randomLetter + "'.";
 
         // Ensure 'fortuneResult' is passed, not 'name'
         window.location.href = "result.html?fortuneResult=" + encodeURIComponent(randomMessage);
